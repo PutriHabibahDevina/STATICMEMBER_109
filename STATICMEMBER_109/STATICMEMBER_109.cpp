bc679a8a1733d21580af4ba0c176e2230f79cd98
PutriHabibahDevina <putrihdm13@gmail.com>
2023-05-15 11:29:49 +0700

    membuat class mahasiswa dan deklarasinya

--- STATICMEMBER_109/STATICMEMBER_109.cpp
+++ STATICMEMBER_109/STATICMEMBER_109.cpp
@@ -2,3 +2,15 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+
