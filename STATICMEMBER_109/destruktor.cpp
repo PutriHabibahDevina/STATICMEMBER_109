3eef309aa37e085a7331dcfb40f2f9d6dfb720ef
PutriHabibahDevina <putrihdm13@gmail.com>
2023-05-15 14:27:01 +0700

    menggunakan method constructor untuk memberi nilai awal pada member-membernya

---
+++ STATICMEMBER_109/destruktor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int *arr;
+	int panjang;
+public:
+	angka(int);	//Constructor
+	~angka();	//Destructor
+	void cetakData();
+	void isiData();
+};
+
+//Definisi member function
+angka::angka(int i) {	//Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
